9a863d2af369c92d73042201f75757e04ffa1e8b
Benjamin <benjaminrowe0@gmail.com>
2021-12-24 18:16:13 +1100

    have solved the problem

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name:"Ben",
+    streetAddress: "123, rockmac, vic, 2121"
+}
+
+let updatingEmployee = updateEmployeeWithKeyAndValue = (employee, key, vaule) => {
+    const copyOfEmployeeObj = Object.assign({},employee)
+    copyOfEmployeeObj[key] = vaule
+    return copyOfEmployeeObj
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, vaule){
+    employee[key] = vaule
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const copyOfEmployeeObj = Object.assign({},employee)
+    delete copyOfEmployeeObj[key]
+    return copyOfEmployeeObj
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+}
+
+console.log(updatingEmployee(employee,"streetAddress","11 broadway"))
+console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "josh"))
